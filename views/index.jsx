81d55952925bd7797f34d0aa3b36dfaf7d83aac1
FND <fnd@localhost.localdomain>
2017-06-01 21:15:11 +0200

    added Boostrap

--- views/index.jsx
+++ views/index.jsx
@@ -1,8 +1,14 @@
 import "./layout";
 import renderer, { registerMacro, createElement } from "complate-stream";
 
+const STYLESHEETS = [{
+	uri: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+	hash: "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+}];
+
 registerMacro("site-index", ({ title }) => {
-	return <default-layout title={title}>
+	return <default-layout title={title} stylesheets={STYLESHEETS}
+			body-class="container-fluid">
 		<h1>{title}</h1>
 	</default-layout>;
 });
--- views/layout.jsx
+++ views/layout.jsx
@@ -4,9 +4,13 @@ registerMacro("default-layout", (params, ...children) => <html>
 	<head>
 		<meta charset="utf-8" />
 		<title>{params.title}</title>
+		{(params.stylesheets || []).map(({ uri, hash }) => {
+			return <link rel="stylesheet" href={uri}
+					integrity={hash} crossorigin="anonymous" />;
+		})}
 	</head>
 
-	<body>
+	<body class={params["body-class"]}>
 		{children}
 	</body>
 </html>);
