4e21cdf65913b7ef48cbd0b4c5ae8f8c9dd2a483
FND <fnd@localhost.localdomain>
2017-06-01 21:41:59 +0200

    added style-sheets macro

--- views/layout.jsx
+++ views/layout.jsx
@@ -4,13 +4,21 @@ registerMacro("default-layout", (params, ...children) => <html>
 	<head>
 		<meta charset="utf-8" />
 		<title>{params.title}</title>
-		{(params.stylesheets || []).map(({ uri, hash }) => {
-			return <link rel="stylesheet" href={uri}
-					integrity={hash} crossorigin="anonymous" />;
-		})}
+		<style-sheets items={params.stylesheets} />
 	</head>
 
 	<body class={params["body-class"]}>
 		{children}
 	</body>
 </html>);
+
+registerMacro("style-sheets", ({ items }) => {
+	if(!items || !items.length) {
+		return;
+	}
+
+	return items.map(({ uri, hash }) => {
+		return <link rel="stylesheet" href={uri}
+				integrity={hash} crossorigin="anonymous" />;
+	});
+});
