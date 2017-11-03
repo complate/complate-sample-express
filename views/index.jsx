98ef286a8b795fe003c747cb77b76deeb4ae8bb1
FND <fnd@localhost.localdomain>
2017-11-03 13:40:22 +0100

    upgraded dependencies

--- views/index.jsx
+++ views/index.jsx
@@ -1,16 +1,16 @@
 import ContentPanel from "./panel";
 import ListGroup from "./list";
 import DefaultLayout from "./layout";
-import renderer, { createElement } from "complate-stream";
+import Renderer, { createElement } from "complate-stream";
 
 const STYLESHEETS = [{
 	uri: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
 	hash: "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
 }];
 
-let { renderView, registerView } = renderer("<!DOCTYPE html>");
+let renderer = new Renderer("<!DOCTYPE html>");
 
-registerView(function SiteIndex({ title }) {
+renderer.registerView(function SiteIndex({ title }) {
 	return <DefaultLayout title={title} // eslint-disable-next-line indent
 			stylesheets={STYLESHEETS} body-class="container-fluid">
 		<h1>{title}</h1>
@@ -29,4 +29,6 @@ registerView(function SiteIndex({ title }) {
 	</DefaultLayout>;
 });
 
-export default renderView;
+export default (view, params, stream, fragment, callback) => {
+	return renderer.renderView(view, params, stream, { fragment }, callback);
+};
