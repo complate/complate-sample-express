d4afa7c78f7c71dc7814a918e1b810638438dce9
FND <fnd@localhost.localdomain>
2017-11-04 17:59:59 +0100

    added missing content type

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.use(complate(path.resolve(__dirname, "../dist/bundle.js")));
 
 app.get("/", (req, res) => {
 	res.status(200);
+	res.set("Content-Type", "text/html");
 	res.complate("SiteIndex", { title: "Hello World" }, false);
 });
 
